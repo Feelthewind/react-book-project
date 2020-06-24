f7050a9d8e44c1ced5b2be228e32f72f531d875d
임창수 <skyisthelimitless@gmail.com>
2020-06-24 18:18:35 +0900

    로그인 상태 유지하기

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Responsive from './Responsive';
 import Button from './Button';
 
@@ -30,15 +31,29 @@ const Spacer = styled.div`
     height: 4rem;
 `;
 
-const Header = () => {
+const UserInfo = styled.div`
+    font-weight: 800;
+    margin-right: 1rem;
+`
+
+const Header = ({ user }) => {
     return (
         <>
             <HeaderBlock>
                 <Wrapper>
-                    <div className="logo">REACTERS</div>
-                    <div className="right">
-                        <Button to="/login">로그인</Button>
-                    </div>
+                    <Link to="/" className="logo">
+                        REACTERS
+                    </Link>
+                    {user ? (
+                        <div className="right">
+                            <UserInfo>{user.username}</UserInfo>
+                            <Button>로그아웃</Button>
+                        </div>
+                    ) : (
+                        <div className="right">
+                            <Button to="/login">로그인</Button>
+                        </div>
+                    )}
                 </Wrapper>
             </HeaderBlock>
             <Spacer />
