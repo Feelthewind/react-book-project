667259c48ed37aa8f89a1204433e6448583f2110
임창수 <skyisthelimitless@gmail.com>
2020-06-24 16:48:17 +0900

    회원가입 에러처리

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AuthForm from '../../components/auth/AuthForm';
 import { useDispatch, useSelector } from 'react-redux';
 import { initializeForm, changeField, register } from '../../modules/auth';
@@ -6,6 +6,7 @@ import { check } from '../../modules/user';
 import { withRouter } from 'react-router-dom';
 
 const RegisterForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.register,
@@ -28,8 +29,23 @@ const RegisterForm = ({ history }) => {
     const onSubmit = e => {
         e.preventDefault();
         const { username, password, passwordConfirm } = form;
+        // 하나라도 비어 있다면
+        if ([username, password, passwordConfirm].includes('')) {
+            setError('빈 칸을 모두 입력하세요.');
+            return;
+        }
         if (password !== passwordConfirm) {
-            // TODO: 오류 처리
+            setError('비밀번호가 일치하지 않습니다.');
+            dispatch(changeField({
+                form: 'register',
+                key: 'password',
+                value: '',
+            }));
+            dispatch(changeField({
+                form: 'register',
+                key: 'passwordConfirm',
+                value: '',
+            }));
             return;
         }
         dispatch(register({ username, password }))
@@ -41,8 +57,13 @@ const RegisterForm = ({ history }) => {
 
     useEffect(() => {
         if (authError) {
-            console.log('오류 발생');
-            console.log(authError);
+            // 계정명이 이미 존재할 때
+            if (authError.response.status === 409) {
+                setError('이미 존재하는 계정명입니다.');
+                return;
+            }
+            // 기타 이유
+            setError('회원가입 실패');
             return;
         }
         if (auth) {
@@ -64,6 +85,7 @@ const RegisterForm = ({ history }) => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     )
 }
